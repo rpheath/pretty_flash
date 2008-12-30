8000ffaaf3430cb6f44812cc2ef31567b9c9fc93
rpheath <rpheath@gmail.com>
2008-12-30 12:31:12 -0500

    added installation instructions

--- install.rb
+++ install.rb
@@ -1,1 +1,23 @@
-# Install hook code here
+message = <<-MESSAGE
+  
+  ********************************************
+  To get started, navigate to your project
+  root, and type:
+  
+  $ rake pretty_flash:install
+  
+  That will copy all of the necessary assets
+  to their appropriate homes.
+  
+  !! WARNING: there is no protection against
+     file collision, so your files will be
+     overwritten if they already exist. You
+     may want to check that first! (see the
+     pretty_flash/assets/ to check)
+  
+  Enjoy!
+  ********************************************
+  
+MESSAGE
+
+puts message
